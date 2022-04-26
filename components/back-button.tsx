f8c444a2b328a6e8bb941693c49fdb4d4b560a62
Tony Hazama <tonyhazama4721@gmail.com>
2022-04-26 20:48:10 +0700

    typechecked BookCard

--- components/back-button.tsx
+++ components/back-button.tsx
@@ -4,10 +4,10 @@ import {RiArrowLeftSLine} from 'react-icons/ri';
 
 interface Props {
   onBack: () => void;
-  title: string;
+  title?: string;
 }
 
-export default function BackButton({onBack, title = ""}: Props) {
+export default function BackButton({onBack, title = "Back"}: Props) {
   return (
     <div className="flex items-center mb-4 cursor-pointer" style={{width: "fit-content"}} onClick={onBack}>
       <RiArrowLeftSLine className="mr-4 cursor-pointer text-2xl" />
